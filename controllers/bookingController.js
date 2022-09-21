4f8ddf8fcb5799709924b6746cc322b7e689720d
kehindebillions <85681159+kehindebillions@users.noreply.github.com>
2022-09-21 05:33:56 +0100

    improved stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -22,11 +22,19 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     client_reference_id: req.params.tourId,
     line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
-        amount: tour.price * 100,
-        currency: 'usd',
+        price_data: {
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`
+            ]
+          },
+          currency: 'usd',
+          unit_amount: tour.price * 100
+        },
         quantity: 1
       }
     ]
@@ -52,7 +60,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.line_items[0].unit_amount / 100;
   await Booking.create({ tour, user, price });
 };
 
